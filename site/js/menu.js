cffdec87a8679633225a5de60f425a3785669b32
itggot-Bill-Falk <bill.falk@itggot.se>
2018-03-07 18:30:47 +0100

    Har lagt till en hamburgarmeny och försökt rädda projektet med några mediaqueries

--- site/js/menu.js
+++ site/js/menu.js
@@ -1,4 +1,7 @@
 function toggleMenu(){
-    var element = document.querySelector(".menu");
+    var element = document.querySelector(".hamburgare");
     element.classList.toggle("show");
+
+    element = document.querySelector("main");
+    element.classList.toggle("fade")
 }
